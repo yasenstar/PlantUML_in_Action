fe9c900a9149bd012f7cb240928f9a004fa42331
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-05-25 15:01:34 -0400

    initialization

---
+++ PlantUML.mm
@@ -0,0 +1,129 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="PlantUML" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1716662380897" LINK="https://plantuml.com/" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sky_element_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.25 pt;0 pt;" ENDINCLINATION="116.25 pt;28.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="00 Quick Start" POSITION="bottom_or_right" ID="ID_1426358653" CREATED="1716662000256" MODIFIED="1716662387157" LINK="https://plantuml.com/starting">
+<node TEXT="Local Installation" ID="ID_684281566" CREATED="1716662966846" MODIFIED="1716663000371">
+<node TEXT="installation FAQ" ID="ID_903936578" CREATED="1716663050706" MODIFIED="1716663062075" LINK="https://plantuml.com/faq-install"/>
+</node>
+<node TEXT="Command Line Operations" ID="ID_170625412" CREATED="1716663009148" MODIFIED="1716663016786">
+<node TEXT="Create &quot;Hello World&quot; in Sequence Diagram" ID="ID_1668323427" CREATED="1716663016789" MODIFIED="1716663029606"/>
+</node>
+<node TEXT="Some resources" ID="ID_927664160" CREATED="1716662411299" MODIFIED="1716662420065">
+<node TEXT="PlantUML Project in GitHub" ID="ID_764004615" CREATED="1716662494639" MODIFIED="1716662505348" LINK="https://github.com/plantuml/plantuml"/>
+<node TEXT="PlantUML Language Reference Guide" ID="ID_1495801307" CREATED="1716662517632" MODIFIED="1716662534484" LINK="https://pdf.plantuml.net/1.2020.22/PlantUML_Language_Reference_Guide_en.pdf"/>
+<node TEXT="PlanUML Web Server" ID="ID_1810892035" CREATED="1716662420069" MODIFIED="1716662436141" LINK="https://www.plantuml.com/"/>
+<node TEXT="PlantUML cheatsheet" ID="ID_728879122" CREATED="1716662545213" MODIFIED="1716662554518" LINK="https://devhints.io/plantuml"/>
+<node TEXT="PlantUML 101" ID="ID_254962759" CREATED="1716662745211" MODIFIED="1716662768731" LINK="https://www.webdevtutor.net/blog/plantuml-101-tutorial"/>
+<node TEXT="PlantUML Beginner&apos;s Guide" ID="ID_1570246169" CREATED="1716662940852" MODIFIED="1716662959471" LINK="https://www.webdevtutor.net/blog/beginner-guide-plantuml-sequence-diagrams-with-examples"/>
+<node TEXT="Pladitor - out-of-the-box PlantUML diagram editor" ID="ID_1664797089" CREATED="1716662851053" MODIFIED="1716662880833" LINK="https://plantumleditor.com/"/>
+<node TEXT="Ashley&apos;s PlantUML Doc" ID="ID_1760503818" CREATED="1716662895097" MODIFIED="1716662911066" LINK="https://plantuml-documentation.readthedocs.io/en/latest/index.html"/>
+</node>
+</node>
+<node TEXT="01 Sequence Diagram" POSITION="bottom_or_right" ID="ID_871591616" CREATED="1716658484028" MODIFIED="1716658597532"/>
+<node TEXT="02 Use Case Diagram" POSITION="bottom_or_right" ID="ID_1749037007" CREATED="1716658601040" MODIFIED="1716658606201"/>
+<node TEXT="03 Class Diagram" POSITION="bottom_or_right" ID="ID_1534579784" CREATED="1716658606406" MODIFIED="1716658611130"/>
+<node TEXT="04 Object Diagram" POSITION="bottom_or_right" ID="ID_409306097" CREATED="1716658613306" MODIFIED="1716658754276"/>
+<node TEXT="05 Activity Diagram (legacy)" POSITION="bottom_or_right" ID="ID_1755393633" CREATED="1716658754444" MODIFIED="1716658762019"/>
+<node TEXT="06 Activity Diagram (New Syntax)" POSITION="bottom_or_right" ID="ID_345095188" CREATED="1716658762353" MODIFIED="1716658775583"/>
+<node TEXT="07 Component Diagram" POSITION="bottom_or_right" ID="ID_1822778668" CREATED="1716658775733" MODIFIED="1716658785460"/>
+<node TEXT="08 Deployment Diagram" POSITION="bottom_or_right" ID="ID_1574141853" CREATED="1716658785650" MODIFIED="1716658790360"/>
+<node TEXT="09 State Diagram" POSITION="bottom_or_right" ID="ID_98532341" CREATED="1716658790775" MODIFIED="1716658797956"/>
+<node TEXT="10 Timing Diagram" POSITION="bottom_or_right" ID="ID_1726192803" CREATED="1716658798143" MODIFIED="1716658805350"/>
+<node TEXT="11 Display JSON Data" POSITION="bottom_or_right" ID="ID_588971608" CREATED="1716658805537" MODIFIED="1716658810975"/>
+<node TEXT="12 Display YAML Data" POSITION="bottom_or_right" ID="ID_430003065" CREATED="1716658813635" MODIFIED="1716658819766"/>
+<node TEXT="13 Network Diagram (nwdiag)" POSITION="bottom_or_right" ID="ID_1607808120" CREATED="1716658821719" MODIFIED="1716658834915"/>
+<node TEXT="14 Salt (Wireframe)" POSITION="bottom_or_right" ID="ID_469160178" CREATED="1716658835390" MODIFIED="1716658842755"/>
+<node TEXT="15 Archimate Diagram" POSITION="bottom_or_right" ID="ID_1220791201" CREATED="1716658842919" MODIFIED="1716658851311"/>
+<node TEXT="16 Gantt Diagram" POSITION="bottom_or_right" ID="ID_1346116652" CREATED="1716658851471" MODIFIED="1716658858170"/>
+<node TEXT="17 Mindmap" POSITION="bottom_or_right" ID="ID_1016353481" CREATED="1716658858336" MODIFIED="1716658864947"/>
+<node TEXT="18 Work Breakdown Structure (WBS)" POSITION="bottom_or_right" ID="ID_1852462131" CREATED="1716658865221" MODIFIED="1716658881382"/>
+<node TEXT="19 Maths" POSITION="bottom_or_right" ID="ID_351353212" CREATED="1716658887503" MODIFIED="1716658890721"/>
+<node TEXT="20 Entity Relationship Diagram" POSITION="bottom_or_right" ID="ID_617515665" CREATED="1716658890904" MODIFIED="1716658903227"/>
+<node TEXT="21 Common Commands in PlantUML" POSITION="bottom_or_right" ID="ID_793883633" CREATED="1716658903496" MODIFIED="1716658912203"/>
+<node TEXT="22 Creole" POSITION="bottom_or_right" ID="ID_816118123" CREATED="1716658917930" MODIFIED="1716658923516"/>
+<node TEXT="23 Defining the using sprites" POSITION="bottom_or_right" ID="ID_1151666197" CREATED="1716658925872" MODIFIED="1716658935291"/>
+<node TEXT="24 Skinparam command" POSITION="bottom_or_right" ID="ID_1854221921" CREATED="1716658936241" MODIFIED="1716658943002"/>
+<node TEXT="25 Preprocessing" POSITION="bottom_or_right" ID="ID_1111858564" CREATED="1716658948339" MODIFIED="1716658953613"/>
+<node TEXT="26 Unicode" POSITION="bottom_or_right" ID="ID_948088624" CREATED="1716658953769" MODIFIED="1716658957237"/>
+<node TEXT="27 PlantUML Standard Library" POSITION="bottom_or_right" ID="ID_1504198015" CREATED="1716658957537" MODIFIED="1716658964841"/>
+</node>
+</map>
